6c98d0333e3599094bdd4f0f31d7bbb8b3c6fd4c
Martin Kalábek <martin.kalabek@gmail.com>
2016-05-03 20:53:42 +0200

    Create zip from build

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,12 @@ var gulp = require('gulp');
 var symdest = require('gulp-symdest');
 var electron = require('gulp-atom-electron');
 var clean = require('gulp-clean');
+var zip = require('gulp-vinyl-zip');
 
 var electronVersion = '0.37.8'
 
 gulp.task('default', ['build:win']);
-gulp.task('build', ['clean', 'build:win', 'build:darwin', 'build:linux']);
+gulp.task('build', ['clean', 'build:win', 'build:linux']);
 
 gulp.task('clean', function() {
   return gulp.src('build', {read: false}).pipe(clean());
@@ -20,7 +21,8 @@ gulp.task('build:win', function () {
     arch: 'x64',
     winIcon: 'assets/icon.ico'
   }))
-  .pipe(symdest('build/win'));
+  .pipe(symdest('build/win32-x64'))
+  .pipe(zip.dest('build/win32-x64.zip'));
 });
 
 gulp.task('build:darwin', function () {
@@ -31,7 +33,8 @@ gulp.task('build:darwin', function () {
     arch: 'x64',
     darwinIcon: 'assets/icon.icns'
   }))
-  .pipe(symdest('build/darwin'));
+  .pipe(symdest('build/darwin-x64'))
+  .pipe(zip.dest('build/darwin-x64.zip'));
 });
 
 gulp.task('build:linux', function () {
@@ -41,5 +44,6 @@ gulp.task('build:linux', function () {
     platform: 'linux',
     arch: 'x64'
   }))
-  .pipe(symdest('build/linux'));
+  .pipe(symdest('build/linux-x64'))
+  .pipe(zip.dest('build/linux-x64.zip'));
 });
